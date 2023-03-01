d1a7cc4fd62461e0a6fed01f611a7de0da4ade3e
hariprasath.krishnamoorthy@ideas2it.com <hariprasath.krishnamoorthy@ideas2it.com>
2023-03-02 00:12:58 +0530

    headless config

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
-    headless: false,
+    headless: true,
     navigationTimeout: 60000,
   },
 
